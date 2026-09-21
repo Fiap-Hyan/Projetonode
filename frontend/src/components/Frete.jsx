047699b3301eb032b9c57858f1a97fbfebae1608
Hyan <hyan.ha3@gmail.com>
2026-09-21 09:43:43 -0300

    style: ajustando o visual com tailwind

--- frontend/src/components/Frete.jsx
+++ frontend/src/components/Frete.jsx
@@ -46,12 +46,12 @@ const Frete = () => {
 
 
     return (
-        <div>
-            <div>
-                <h1>Calculadora de Frete</h1>
-                <form>
-                    <div>
-                        <label>Distância(km)</label>
+        <div className="flex justify-center items-center min-h-screen bg-gray-200 p-4">
+            <div className="bg-amber-100 p-8 rounded-2xl shadow-2xl w-full">
+                <h1 className="text-3xl font-bold text-blue-950 mb-6">Calculadora de Frete</h1>
+                <form onSubmit={handleFrete} className="space-y-6">
+                    <div className="space-y-2 text-left">
+                        <label className="block text-gray-700 font-medium">Distância(km)</label>
                         <input
                             type="number"
                             id="distancia"
@@ -60,21 +60,28 @@ const Frete = () => {
                             step="0.01"
                             required
                             onChange={(e) => setDistancia(e.target.value)}
+                            className="w-full px-4 py-3 border-gray-400 rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500 "
                         />
                     </div>
-                    <div>
-                        <label>Transporte</label>
+                    <div className="space-y-2 text-left">
+                        <label className="block text-gray-700 font-medium">Transporte</label>
                         <select
                             id="transport"
                             value={tipoTransporte}
                             onChange={(e) => setTipoTransporte(e.target.value)}
+                            className="w-full px-4 py-3 border-gray-400 rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500 "
                         >
                             <option value="bicicleta">Bicicleta</option>
                             <option value="carro">Carro</option>
                             <option value="drone">Drone</option>                     </select>
                     </div>
 
-                    <button type="submit" disabled={loading}>
+                    <button
+                        type="submit"
+                        disabled={loading}
+                        className="w-full bg-blue-700 text-white font-bold py-3 rounded-2xl hover:bg-blue-900 transition-colors disabled:bg-gray-400 disabled:cursor-not-allowed"
+
+                    >
                         {loading ? "Calculando" : "Calcular"}
                     </button>
                 </form>
@@ -90,5 +97,6 @@ const Frete = () => {
         </div>
     )
 }
+
 
 export default Frete
