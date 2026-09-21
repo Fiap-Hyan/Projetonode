173ec21d676556f0bd78ddc5838aff4e60ac4147
Hyan <hyan.ha3@gmail.com>
2026-09-21 09:13:33 -0300

    feat: funcionalidades frete

--- frontend/src/components/Frete.jsx
+++ frontend/src/components/Frete.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 const Frete = () => {
 
     // Hooks - useState - manipula o estado da variavel 
-    const [distancia, setDistancia] = useState();
+    const [distancia, setDistancia] = useState('');
     const [tipoTransporte, setTipoTransporte] = useState('bicicleta');
     const [valorFrete, setValorFrete] = useState(null);
     const [loading, setLoading] = useState(false);
@@ -44,11 +44,51 @@ const Frete = () => {
         }
     }
 
-  return (
-    <>
-      
-    </>
-  )
+
+    return (
+        <div>
+            <div>
+                <h1>Calculadora de Frete</h1>
+                <form>
+                    <div>
+                        <label>Distância(km)</label>
+                        <input
+                            type="number"
+                            id="distancia"
+                            value={distancia}
+                            min="0"
+                            step="0.01"
+                            required
+                            onChange={(e) => setDistancia(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label>Transporte</label>
+                        <select
+                            id="transport"
+                            value={tipoTransporte}
+                            onChange={(e) => setTipoTransporte(e.target.value)}
+                        >
+                            <option value="bicicleta">Bicicleta</option>
+                            <option value="carro">Carro</option>
+                            <option value="drone">Drone</option>                     </select>
+                    </div>
+
+                    <button type="submit" disabled={loading}>
+                        {loading ? "Calculando" : "Calcular"}
+                    </button>
+                </form>
+
+                {error && <p>{error}</p>}
+
+                {valorFrete !== null && (
+                    <div>
+                        <h2>valor do Frete: R$ {valorFrete}</h2>
+                    </div>
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default Frete
